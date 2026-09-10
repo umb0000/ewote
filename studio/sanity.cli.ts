23fbfa4516319d1f712e830248539ff5c91515c1
ewote <the777the7@gmail.com>
2026-09-10 13:05:35 +0900

    Configure Sanity Studio deployment

--- studio/sanity.cli.ts
+++ studio/sanity.cli.ts
@@ -2,4 +2,6 @@ import { defineCliConfig } from 'sanity/cli';
 
 export default defineCliConfig({
   api: { projectId: 'zb6cjjh8', dataset: 'production' },
+  studioHost: 'ewote-portfolio',
+  deployment: { appId: 'ab2qzo807ebgy1zzp6ffh2ph' },
 });
